9288dbb64ead88b4339439edf5324549ab228ce8
yiruzu <94981325+yiruzu@users.noreply.github.com>
2023-03-24 20:39:44 +0100

    Fixed Code

--- server/common.lua
+++ server/common.lua
@@ -4,17 +4,14 @@ ESX.Jobs = {}
 ESX.Items = {}
 Core = {}
 Core.UsableItemsCallbacks = {}
-Core.ServerCallbacks = {}
-Core.ClientCallbacks = {}
-Core.CurrentRequestId = 0
-Core.TimeoutCount = -1
-Core.CancelledTimeouts = {}
 Core.RegisteredCommands = {}
 Core.Pickups = {}
 Core.PickupId = 0
 Core.PlayerFunctionOverrides = {}
-
+Core.DatabaseConnected = false
 Core.playersByIdentifier = {}
+
+Core.vehicleTypesByModel = {}
 
 AddEventHandler("esx:getSharedObject", function(cb)
     cb(ESX) 
@@ -41,6 +38,7 @@ local function StartDBSync()
 end
 
 MySQL.ready(function()
+  Core.DatabaseConnected = true
   if not Config.OxInventory then
     local items = MySQL.query.await('SELECT * FROM items')
     for k, v in ipairs(items) do
@@ -62,25 +60,6 @@ MySQL.ready(function()
     end
   end
 
-  local Jobs = {}
-  local jobs = MySQL.query.await('SELECT * FROM jobs')
-
-  for _, v in ipairs(jobs) do
-    Jobs[v.name] = v
-    Jobs[v.name].grades = {}
-  end
-
-  local jobGrades = MySQL.query.await('SELECT * FROM job_grades')
-
-  for _, v in ipairs(jobGrades) do
-    if Jobs[v.job_name] then
-      Jobs[v.job_name].grades[tostring(v.grade)] = v
-    else
-      print(('[^3WARNING^7] ESX ^5Legacy^0 job %s does not exist'):format(v.job_name))
-    end
-  end
-
-  ESX.Jobs = Jobs
   ESX.RefreshJobs()
 
   print(('[^2INFO^7] ESX ^5Legacy %s^0 initialized!'):format(GetResourceMetadata(GetCurrentResourceName(), "version", 0)))
@@ -96,15 +75,6 @@ AddEventHandler('esx:clientLog', function(msg)
   if Config.EnableDebug then
     print(('[^2TRACE^7] %s^7'):format(msg))
   end
-end)
-
-RegisterServerEvent('esx:triggerServerCallback')
-AddEventHandler('esx:triggerServerCallback', function(name, requestId,Invoke, ...)
-  local source = source
-
-  ESX.TriggerServerCallback(name, requestId, source,Invoke, function(...)
-    TriggerClientEvent('esx:serverCallback', source, requestId,Invoke, ...)
-  end, ...)
 end)
 
 RegisterNetEvent("esx:ReturnVehicleType", function(Type, Request)
